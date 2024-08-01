2c6412f860c065e566eadb0ad56d6b1c0dde1666
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-01 16:01:45 +0000

    make swap opcode

--- opcodes.c
+++ opcodes.c
@@ -57,3 +57,18 @@ void pop(stack_t **stack, unsigned int line_number)
     }
     free(temp);
 }
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+    int temp;
+
+    if (!*stack || !(*stack)->next)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    temp = (*stack)->n;
+    (*stack)->n = (*stack)->next->n;
+    (*stack)->next->n = temp;
+}

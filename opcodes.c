fca92b4ff4caf823e02671d453edc68f85d3326c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-01 16:04:21 +0000

    make add opcode

--- opcodes.c
+++ opcodes.c
@@ -72,3 +72,18 @@ void swap(stack_t **stack, unsigned int line_number)
     (*stack)->n = (*stack)->next->n;
     (*stack)->next->n = temp;
 }
+
+void add(stack_t **stack, unsigned int line_number)
+{
+    int sum;
+
+    if (!*stack || !(*stack)->next)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    sum = (*stack)->n + (*stack)->next->n;
+    pop(stack, line_number);
+    (*stack)->n = sum;
+}

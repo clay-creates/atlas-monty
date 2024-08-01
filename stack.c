2ee47e4b123d89485f086b1338f873e2ebfec713
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-01 16:12:18 +0000

    make stack.c

---
+++ stack.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+void free_stack(stack_t *stack)
+{
+    stack_t *temp;
+
+    while (stack)
+    {
+        temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+}
+
+int is_number(char *str)
+{
+    if (!str || *str == '\0')
+    {
+        return (0);
+    }
+
+    if (*str == '-' || *str == '+')
+    {
+        str++;
+    }
+
+    while (*str)
+    {
+        if (!isdigit(*str))
+        {
+            return (0);
+        }
+        str++;
+    }
+
+    return (1);
+}
